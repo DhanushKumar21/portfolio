9ec33ec1d2d5d316bca9710a0538569e1056c6da
dhanz <dhanzkumar99@gmail.com>
2026-05-01 21:01:26 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Portfolio Loaded");
+const projects = document.querySelectorAll(".project");
+
+window.addEventListener("scroll", () => {
+  projects.forEach(p => {
+    const top = p.getBoundingClientRect().top;
+    if (top < window.innerHeight - 50) {
+      p.style.opacity = 1;
+      p.style.transform = "translateY(0)";
+    }
+  });
+});
